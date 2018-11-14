7a9b3ba32f27a07203b21eee08a21f07cc7a11b2
serii <seriiburduja@gmail.com>
2018-11-14 23:23:20 +0200

    add text for post

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'tkachev');
 define('DB_USER', 'root');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', 'mysql');
+define('DB_PASSWORD', '');
 
 /** Имя сервера MySQL */
 define('DB_HOST', 'localhost');
